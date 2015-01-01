975ad5d2fb8cf899395b09013302fea0a29b82a8
Piotr Solnica <piotr.solnica@gmail.com>
2015-01-01 16:54:13 +0100

    Add initial spec

--- lib/rom-virtus.rb
+++ lib/rom-virtus.rb
@@ -2,4 +2,28 @@ require 'rom'
 require 'virtus'
 
 module ROM
+  class Setup
+    def from_virtus(virtus_model)
+      name = Inflecto.pluralize(Inflecto.underscore(virtus_model.name.to_s)).to_sym
+      attributes = virtus_model.attribute_set
+
+      schema do
+        base_relation(name) do
+          repository :default
+
+          attributes.each do |attr|
+            attribute attr.name
+          end
+        end
+      end
+
+      relation(name)
+
+      mappers do
+        define(name) do
+          model virtus_model
+        end
+      end
+    end
+  end
 end
--- spec/integration/setup_spec.rb
+++ spec/integration/setup_spec.rb
@@ -3,11 +3,27 @@ require 'spec_helper'
 describe 'Setup ROM from Virtus models' do
   it 'works' do
     class User
-      include Virtus.model
+      include Virtus.value_object
 
-      attribute :id, Integer
-      attribute :name, String
-      attribute :age, Integer
+      values do
+        attribute :id, Integer
+        attribute :name, String
+        attribute :age, Integer
+      end
     end
+
+    setup = ROM.setup("memory://localhost/test")
+
+    setup.from_virtus(User)
+
+    rom = setup.finalize
+
+    expect(rom.relations.users).not_to be(nil)
+
+    rom.schema.users << { id: 1, name: 'Piotr', age: 31 }
+
+    user = User.new(id: 1, name: 'Piotr', age: 31)
+
+    expect(rom.read(:users)).to match_array([user])
   end
 end
